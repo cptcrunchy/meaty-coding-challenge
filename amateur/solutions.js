bbbb28863fd0df7dd89549df0736b1a095448292
Jason Gutierrez <jasong84@gmail.com>
2019-03-08 09:08:18 -0600

    Added notation for quokka and lines for question 3
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring production code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    docs     (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    chore    (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Capitalize the subject line
    #    Use the imperative mood in the subject line
    #    Do not end the subject line with a period
    #    Separate subject from body with a blank line
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------

--- amateur/solutions.js
+++ amateur/solutions.js
@@ -1,3 +1,10 @@
+/*
+    You may notice that I have //? after I invoke each function.
+    I'm using a VSCode plugin called Quokka. https://quokkajs.com/
+    Its free to use and great when you have to evaluate your code
+    and you don't want to use console.log on everything.
+*/
+
 // This is a helper function to get the sum of values in an array of integers
 const addNumbers = (a, b) => a + b
 
@@ -18,6 +25,7 @@ const getSumOfSequence = (start, finish, step) => {
 
 // getSumOfSequence(4, 10, 5) //?
 
+// Challenge 2
 const doesStringContainKeyWord = (str) => {
     const PhraseRegex = /(english)+/gi;
     return PhraseRegex.test(str)
@@ -26,23 +34,29 @@ const doesStringContainKeyWord = (str) => {
 // doesStringContainKeyWord("SMFEnglishasnD")//?
 // doesStringContainKeyWord("asdneglihsth")//?
 
-
+// Challenge 3
 const getArraySumOfPositiveAndNegativeNums = (arrayOfNums) => {
     // First check if parameter is an array
     if (!Array.isArray(arrayOfNums)) return []
 
-    // We are using the rest and spread operator for line 35.
-    // The reduce method uses a callback function which passes an array with 2 values, p (short for positive) and n (short for negative)
-    // And each value in the arrayOfNums to be evaluated.
+    /*  
+        We are using the rest and spread operator for line 47, 49 & 51.
+        The reduce method uses a callback function which passes an array with 2 values, p (short for positive) and n (short for negative)
+        And each value in the arrayOfNums to be evaluated.
+    */
     const [posNum, negNum] = arrayOfNums.reduce(([p,n], e) => (e > 0 ? 
         // If the value is greater than 0 then we add it to the posNum array
         [[...p,e],n] :
         // Else we add it to the negNum array. We then initiate them with a two dimensional array or an array of arrays.
         [p,[...n, e]]), [[],[]])
-    // The quesion wants the count of positive numbers and the SUM of the negative numbers.
-    // If you got this question wrong, it was because of this simple oversight.
+    /*  
+        The quesion wants the count of positive numbers and the SUM of the negative numbers.
+        If you got this question wrong, it was because of this simple oversight.
+    */
     const countOfPosNums = posNum.length , sumOfNegNums = negNum.reduce(addNumbers)
     return [countOfPosNums,sumOfNegNums]
 }
 
 getArraySumOfPositiveAndNegativeNums([-1,1,2,-3,5,2,-10])//?
+
+
